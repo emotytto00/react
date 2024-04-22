e4e4663fb52bb71a79c7d89a05aef2ca888228d1
emotytto00 <johannes.liikanen@gmail.com>
2024-04-22 11:03:13 +0300

    almost redi

--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -1,32 +1,27 @@
 import PropTypes from 'prop-types';
+import {Link} from 'react-router-dom';
 
-const MediaRow = (props) => {
-  const {item, setSelectedItem} = props;
-
-  const handleClick = () => {
-    setSelectedItem(item);
-  }
-
-  return (
-    <tr key={item.media_id}>
-              <td>
-                <img src={item.thumbnail} alt={item.title} />
-              </td>
-              <td>{item.title}</td>
-              <td>{item.description}</td>
-              <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
-              <td>{item.filesize}</td>
-              <td>{item.media_type}</td>
-              <td>
-                <button onClick={handleClick}>View</button>
-              </td>
-            </tr>
-  );
-};
+const MediaRow = ({item}) => (
+  <tr>
+    <td>
+      <img src={item.thumbnail} alt={item.title} />
+    </td>
+    <td>{item.username}</td>
+    <td>{item.title}</td>
+    <td>{item.description}</td>
+    <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
+    <td>{item.filesize}</td>
+    <td>{item.media_type}</td>
+    <td>
+      <Link to={`/media/${item.media_id}`} state={{item}}>
+        View linkki
+      </Link>
+    </td>
+  </tr>
+);
 
 MediaRow.propTypes = {
   item: PropTypes.object.isRequired,
-  setSelectedItem: PropTypes.func.isRequired,
 };
 
 export default MediaRow;
